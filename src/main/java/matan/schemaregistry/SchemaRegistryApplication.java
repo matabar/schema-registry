15326140b8083e78daaeaaade19d28408ed9a674
Matab Baruch <matan.baruch.h8@gmail.com>
2023-05-02 22:58:34 +0300

    Initialize using spring initializer

---
+++ src/main/java/matan/schemaregistry/SchemaRegistryApplication.java
@@ -0,0 +1,13 @@
+package matan.schemaregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchemaRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchemaRegistryApplication.class, args);
+	}
+
+}
